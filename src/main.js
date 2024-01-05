038f314a992c1c5a22ed34ab7fa660dadf0109ac
JongHoPark <qkrwhdgh126@gmail.com>
2024-01-05 14:58:22 +0900

    feat: localstorage 기본 세팅

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-const a = 'bigman';
+const todoInput = document.getElementById('todo-input');
+const submitBtn = document.getElementById('submit-btn');
+
+submitBtn.addEventListener('click', () => {
+  console.log('click');
+  localStorage.setItem('title', todoInput.value);
+});
+
+const getdata = localStorage.getItem('title');
+const lst1 = document.querySelector('.lst1');
+lst1.innerHTML = getdata;
