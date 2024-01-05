d366ababa1e2a772c54303391a6b6f12282c5371
JongHoPark <qkrwhdgh126@gmail.com>
2024-01-05 15:05:10 +0900

    feat: input 공백 감지 추가

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ const submitBtn = document.getElementById('submit-btn');
 
 submitBtn.addEventListener('click', () => {
   console.log('click');
-  localStorage.setItem('title', todoInput.value);
+  if (todoInput.value.trim() == '') {
+    alert('할 일을 입력해주세요.');
+  } else {
+    localStorage.setItem('todo', todoInput.value);
+  }
 });
 
-const getdata = localStorage.getItem('title');
+const getdata = localStorage.getItem('todo');
+
 const lst1 = document.querySelector('.lst1');
 lst1.innerHTML = getdata;
