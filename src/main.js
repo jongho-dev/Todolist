6a493194df05de11767b701c3897e72b16a4e30f
JongHoPark <qkrwhdgh126@gmail.com>
2024-01-08 17:01:03 +0900

    feat: localstorage 수정

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
 const todoInput = document.getElementById('todo-input');
 const submitBtn = document.getElementById('submit-btn');
 
+const lst1 = document.querySelector('.lst1');
+const keys = Object.keys(window.localStorage);
+console.log(keys);
+console.log(keys.includes('todo'));
+if (keys.includes('todo')) {
+  const getdata = localStorage.getItem('todo');
+  lst1.innerHTML = getdata;
+} else {
+  const getdata = [];
+  localStorage.setItem('todo', getdata);
+}
 submitBtn.addEventListener('click', () => {
   console.log('click');
   if (todoInput.value.trim() == '') {
     alert('할 일을 입력해주세요.');
   } else {
-    localStorage.setItem('todo', todoInput.value);
+    getdata.append(todoInput.value);
+    localStorage.setItem('todo', getdata);
   }
 });
 
-const getdata = localStorage.getItem('todo');
-
-const lst1 = document.querySelector('.lst1');
-lst1.innerHTML = getdata;
+localStorage.clear();
