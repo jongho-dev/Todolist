bc35d30a9583ea4200ae0a77984c347162f9425f
JongHoPark <qkrwhdgh126@gmail.com>
2024-01-11 23:00:05 +0900

    feat: list 추가 삭제

--- src/main.js
+++ src/main.js
@@ -4,33 +4,37 @@ const todoList = document.getElementById('todo-list');
 
 let listItems = [];
 
-function saveItems() {
-  const obj = {
-    title: todoInput.value,
-  };
-  const json = JSON.stringify(obj);
-  console.log(json);
-  items.push(json);
-  console.log(items);
-  localStorage.setItem('todo', items);
-}
-
 submitBtn.addEventListener('click', () => {
   paintTodo(todoInput.value);
 });
+
+function deleteTodo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  todoList.removeChild(li);
+  const filterItems = listItems.filter((toDo) => {
+    return toDo.id != parseInt(li.id);
+  });
+  listItems = filterItems;
+  localStorage.setItem('todo', JSON.stringify(listItems));
+}
 
 function paintTodo(text) {
   const li = document.createElement('li');
   const span = document.createElement('span');
   const btn = document.createElement('button');
+  const newId = listItems.length + 1;
   span.innerHTML = text;
   btn.innerHTML = 'X';
+  btn.addEventListener('click', deleteTodo);
   li.appendChild(span);
   li.appendChild(btn);
+  li.id = newId;
   todoList.appendChild(li);
 
   const obj = {
     text,
+    id: newId,
   };
   listItems.push(obj);
   localStorage.setItem('todo', JSON.stringify(listItems));
